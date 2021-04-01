bcbf01c92780193efe4762f7f6a6227375534623
ptxs99 <56853999+ptxs99@users.noreply.github.com>
2021-04-01 12:31:56 -0700

    Update hash.py

--- hash.py
+++ hash.py
@@ -6,29 +6,41 @@ import os
 import sys
 import hashlib
 
-ignore_list = ["/dev", "/proc", "/run", "/sys", "/tmp", "/var/lib", "/var/run"]
+ignore_list = ['dev', "proc", "run", "sys", "tmp", "var/lib", "var/run"]
 
 def main():
    for root, dirs, files in os.walk(".", topdown=True): #from geeksforgeeks.org
       # if root not in ignore_list:
+#take out ./
 
+     for item in ignore_list:
+            if item in dirs:
+                dirs.remove(item)
+     print(dirs)
      testlist = root.split("/")
+     print(testlist)
      for directory in testlist:
-         if directory not in ignore_list:
-             print(root)
+         if (directory in ignore_list) and (directory in root.split()):
+
+             print("Skipping Directory: ", directory)
+
+         else:
+             print("Directory being read: ", directory)
+             print("Root: ",root)
              #######STACK OVERFLOW######
              for f in files:
-                current_file = os.path.join(root,f)
-                H = hashlib.sha256()
-                with open(current_file, "rb") as FIN:
-                    H.update(FIN.read())
-                    with open("gethashes.txt", "a+") as myfile:
-                        myfile.write(current_file),myfile.write("\n"),myfile.write("            "),myfile.write(H.hexdigest()),myfile.write("%s\n")
+                 current_file = os.path.join(root,f)
+                 H = hashlib.sha256()
+                 with open(current_file, "rb") as FIN:
+                     H.update(FIN.read())
+                     with open("gethashes.txt", "a+") as myfile:
+                         myfile.write(current_file),myfile.write("\n"),myfile.write("            "),myfile.write(H.hexdigest()),myfile.write("%s\n")
+
             ##############################################
                # print(dirs)
                # print(files)
                # print(hashlib.SHA256( ))
-                print("-----------------------------------------")
+         print("-----------------------------------------")
 
 
 #hashlib.SHA256(file)
@@ -37,3 +49,4 @@ def main():
 
 if __name__ == "__main__":
    main()
+
