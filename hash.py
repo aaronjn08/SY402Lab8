431540a29c43ef263aa453c6d1d12c13e8929d39
aaronjn08 <63920339+aaronjn08@users.noreply.github.com>
2021-04-05 22:15:28 -0400

    Update hash.py

--- hash.py
+++ hash.py
@@ -2,6 +2,13 @@
 #Aaron Noller & Paul Schartung
 #Lab08
 
+
+import datetime
+now = datetime.datetime.now()
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
+
+
 import os
 import sys
 import hashlib
